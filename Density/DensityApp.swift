38c3278f6ff4d8d01653f14dffeb1c571f674bac
Gareth Mansfield <garethmansfield@icloud.com>
2021-01-23 13:07:08 -0500

    Initial Commit

---
+++ Density/DensityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DensityApp.swift
+//  Density
+//
+//  Created by Gareth Mansfield on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct DensityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
